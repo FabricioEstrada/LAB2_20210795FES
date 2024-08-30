6df7e9b8facb6715272a045187a6ebb3f6435a82
FabricioEstrada <a20210795@pucp.edu.pe>
2024-08-29 19:45:57 -0500

    prueba

---
+++ lab2_20210795/src/test/java/com/example/lab2_20210795/Lab220210795ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20210795;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220210795ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
